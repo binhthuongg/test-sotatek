ea721762acb74d30e4a60f48c9e399f225cf4d62
manhnv <manhnv@sunshinegroup.vn>
2021-06-28 22:39:27 +0700

    update, lay duoc gia tri, truoc khi thu validate time

---
+++ src/components/RenderDatePicker/index.js
@@ -0,0 +1,44 @@
+import dayjs from 'dayjs';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function RenderDatePicker(props) {
+	console.log('props', props);
+	console.log('props222');
+	const [startDate, setStartDate] = useState(new Date());
+	const { value, name, input, onChange, isErrorDate } = props;
+	const IsBeforeNow = (dateString) => {
+		if (dayjs(dateString).isBefore(dayjs(), 'day')) {
+			return true;
+		}
+		return false;
+	};
+	const handleChange = (date) => {
+		console.log('date', date);
+		console.log('abc', IsBeforeNow(date));
+		// const date
+		if (IsBeforeNow(date)) {
+			isErrorDate(true);
+			return;
+		} else {
+			isErrorDate(false);
+			onChange(date);
+			setStartDate(date);
+		}
+	};
+	return (
+		<DatePicker
+			placeholderText='Chọn ngày'
+			// dateFormat='yyyy/MM/dd'
+			// dateFormat='dd/MM/yyyy'
+			dateFormat='dd/MM/yyyy'
+			selected={startDate} // needs to be checked if it is valid date
+			disabledKeyboardNavigation
+			name={name}
+			onChange={(date) => handleChange(date)}
+		/>
+	);
+}
+
+export default RenderDatePicker;
--- src/components/TaskSingle/index.js
+++ src/components/TaskSingle/index.js
@@ -1,76 +1,134 @@
-import React, { useState } from "react";
-import { StyledComponent } from "./styles";
-import { Form, Field } from "react-final-form";
-import DatePicker from "react-datepicker";
+import dayjs from 'dayjs';
+import React, { useState } from 'react';
+import 'react-datepicker/dist/react-datepicker.css';
+import { Field, Form } from 'react-final-form';
+import RenderDatePicker from '../RenderDatePicker';
+import { StyledComponent } from './styles';
 
 function TaskSingle(props) {
-  const onSubmit = async values => {
-    window.alert(JSON.stringify(values, 0, 2));
-  };
+	const textError = 'Thời gian không được trước ngày hôm nay';
+	const listPriority = {
+		low: 'low',
+		normal: 'normal',
+		high: 'high',
+	};
 
-  const [startDate, setStartDate] = useState(new Date());
+	const [IsShowErrorPickTimeBeforeNow, setIsShowErrorPickTimeBeforeNow] =
+		useState(false);
 
-  return (
-    <StyledComponent>
-      <Form
-        onSubmit={onSubmit}
-        validate={values => {
-          const errors = {};
-          if (!values.username) {
-            errors.username = "Required";
-          }
-          return errors;
-        }}
-        render={({ handleSubmit, form, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit}>
-            <Field name="username">
-              {({ input, meta }) => (
-                <div>
-                  <label>Title</label>
-                  <input {...input} type="text" placeholder="Add new task" />
-                  {meta.error && meta.touched && <span>{meta.error}</span>}
-                </div>
-              )}
-            </Field>
-            <Field name="password">
-              {({ input, meta }) => (
-                <div>
-                  <label>Description</label>
-                  <textarea {...input} type="textarea" rows="10" />
-                  {meta.error && meta.touched && <span>{meta.error}</span>}
-                </div>
-              )}
-            </Field>
-            <div className="row">
-              <div className="col-sm-6">
-                <Field name="dueDate">
-                  {({ input, meta }) => (
-                    <div>
-                      <label>Due date</label>
-                      <DatePicker {...input} selected={startDate} />
-                      {meta.error && meta.touched && <span>{meta.error}</span>}
-                    </div>
-                  )}
-                </Field>
-              </div>
-              <div className="col-sm-6">
-                <Field name="priority" component="select">
-                  <option value="low"> low</option>
-                  <option value="normal"> normal</option>
-                  <option value="high"> high</option>
-                </Field>
-              </div>
-            </div>
-            <div className="buttons">
-              <button type="submit" disabled={submitting}>
-                Submit
-              </button>
-            </div>
-          </form>
-        )}
-      />
-    </StyledComponent>
-  );
+	const handleErrorDate = (isErrorDate) => {
+		if (isErrorDate) {
+			setIsShowErrorPickTimeBeforeNow(true);
+		} else {
+			setIsShowErrorPickTimeBeforeNow(false);
+		}
+	};
+
+	const onSubmit = async (values) => {
+		setIsShowErrorPickTimeBeforeNow(false);
+		console.log('values', values);
+	};
+
+	const initialFormValue = {
+		username: '',
+		description: '',
+		dueTime: dayjs().format('DD/MM/YYYY'),
+		priority: listPriority.normal,
+	};
+
+	const MyForm = ({ subscription }) => (
+		<Form
+			onSubmit={onSubmit}
+			subscription={subscription}
+			initialValues={initialFormValue}
+			validate={(values) => {
+				const errors = {};
+				if (!values.username) {
+					errors.username = 'Required';
+				}
+				return errors;
+			}}
+			render={({ handleSubmit, form, submitting, pristine, values }) => (
+				<form onSubmit={handleSubmit}>
+					<Field name='username'>
+						{({ input, meta }) => (
+							<div>
+								<label>Title</label>
+								<input
+									{...input}
+									type='text'
+									placeholder='Add new task'
+								/>
+								{!meta.active && meta.error && meta.touched && (
+									<span>{meta.error}</span>
+								)}
+							</div>
+						)}
+					</Field>
+					<Field name='description'>
+						{({ input, meta }) => (
+							<div>
+								<label>Description</label>
+								<textarea
+									{...input}
+									type='textarea'
+									rows='10'
+								/>
+								{!meta.active && meta.error && meta.touched && (
+									<span>{meta.error}</span>
+								)}
+							</div>
+						)}
+					</Field>
+					<div className='row'>
+						<div className='col-sm-6'>
+							<Field name='dueTime'>
+								{({ input, meta }) => {
+									console.log(meta);
+									return (
+										<div>
+											<label>due Time</label>
+											<RenderDatePicker
+												isErrorDate={handleErrorDate}
+												{...input}
+											/>
+											{!meta.active &&
+												IsShowErrorPickTimeBeforeNow && (
+													<span>{textError}</span>
+												)}
+										</div>
+									);
+								}}
+							</Field>
+						</div>
+						<div className='col-sm-6'>
+							<Field name='priority' component='select'>
+								<option value={listPriority.low}>
+									{' '}
+									{listPriority.low}
+								</option>
+								<option value={listPriority.normal}>
+									{listPriority.normal}
+								</option>
+								<option value={listPriority.high}>
+									{listPriority.high}
+								</option>
+							</Field>
+						</div>
+					</div>
+					<div className='buttons'>
+						<button type='submit'>Submit</button>
+					</div>
+				</form>
+			)}
+		/>
+	);
+
+	return (
+		<StyledComponent>
+			<MyForm subscription={{ submitting: true, pristine: true }} />
+		</StyledComponent>
+	);
 }
 
 export default TaskSingle;
